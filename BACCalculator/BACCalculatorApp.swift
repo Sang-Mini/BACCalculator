5fe0eaf674ac0460810e0022fce074ae71efe54b
Sang-Mini <sangmin3062@gmail.com>
2023-09-08 14:51:45 +0900

    Initial Commit

---
+++ BACCalculator/BACCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BACCalculatorApp.swift
+//  BACCalculator
+//
+//  Created by 유상민 on 2023/09/08.
+//
+
+import SwiftUI
+
+@main
+struct BACCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
